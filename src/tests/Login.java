1e3ef5ba1e5bccdff47a2423168763ace5344045
LubnaKE <lubnakhambiye@gmail.com>
2020-05-06 00:51:19 +0800

    add Login.java.

---
+++ src/tests/Login.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class Login {
+
+    public static void main(String[] args) {
+        System.out.println("enter your username: ");
+        System.out.println("enter you password:");
+    }
+
+}
