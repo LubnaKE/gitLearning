5768db84764c4243411f8daa527f39cfd8e555b1
LubnaKE <59313223+LubnaKE@users.noreply.github.com>
2020-05-11 20:52:07 +0800

    Update Person.java

--- src/tests/Person.java
+++ src/tests/Person.java
@@ -5,6 +5,8 @@ public class Person {
     public static void main(String[] args) {
 
         System.out.println("Hello beautiful people of the world!!!");
+        
+        //this is a peaceful time for nature to reboost it's internal energy!!
     }
 
 }
