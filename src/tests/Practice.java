40f3d0b84e5b332c09de31c5d104605493950574
LubnaKE <lubnakhambiye@gmail.com>
2020-05-12 22:45:26 +0800

    add Practice.java

---
+++ src/tests/Practice.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        int x=23;
+        String name ="Mike Smith";
+
+        System.out.println("data= " + x +" "+ name);
+
+
+    }
+}
